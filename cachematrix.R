b188e648e029b0bc4dede694af5583dbaf8efe91
avgilev <avgilev@gmail.com>
2015-04-24 17:58:51 +0500

    Implementation Of Programming Assignment 2
    Implemented two functions: makeCacheMatrix, cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions designed to work together for the purpose of caching 
+## inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function form environment to store (cache) matrix and it`s inverse
+## and constructs the list of functions for set/get matrix and it`s inverse  
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    setMatrix <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    getMatrix <- function() x
+    setInverse <- function(inv) i <<- inv
+    getInverse <- function() i
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function calculate and return inverse of matrix and save it so 
+## subsequent calls skip calculation step.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        return(i)
+    }
+    data <- x$getMatrix()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    ## Return a matrix that is the inverse of 'x'
+    i
 }
